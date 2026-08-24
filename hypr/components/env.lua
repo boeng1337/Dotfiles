96dfb992f57821a8bcf0145c61e142eeab3686c5
Maxim <79853238+boeng1337@users.noreply.github.com>
2026-08-24 12:46:46 +0200

    Delete hypr directory

--- hypr/components/env.lua
+++
@@ -1,6 +0,0 @@
--- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
-
-hl.env("XCURSOR_SIZE", "24")
-hl.env("HYPRCURSOR_SIZE", "24")
-hl.env("XCURSOR_THEME", "catppuccin-mocha-blue-cursors")
-hl.env("XCURSOR_SIZE", "24")
